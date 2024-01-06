91b1a4716f403bda58a919ec0e1789719f9f07c8
msfiqih <msfiqih@gmail.com>
2024-01-07 03:56:23 +0700

    font header resep

--- resources/views/content/print/resep.blade.php
+++ resources/views/content/print/resep.blade.php
@@ -7,8 +7,8 @@
         <img src="{{ 'data:image/jpeg;base64,' . base64_encode($setting->logo) }}" alt="" width="50px" style="left: 10px;position:absolute">
         <div style="text-align: center; margin-left:60px;">
             <p style="margin-bottom: 0px;font-weight: bold;font-size:25px">{{ $setting->nama_instansi }}</p>
-            <p style="font-size: 20px">{{ $setting->alamat_instansi }}, {{ $setting->kabupaten }},{{ $setting->propinsi }}</p>
-            <p style="font-size: 20px">Telp. {{ $setting->kontak }}, Email : {{ $setting->email }} </p>
+            <p style="font-size: 16px">{{ $setting->alamat_instansi }}, {{ $setting->kabupaten }},{{ $setting->propinsi }}</p>
+            <p style="font-size: 16px">Telp. {{ $setting->kontak }}, Email : {{ $setting->email }} </p>
         </div>
         <hr>
         <div class="info" style="margin-bottom:20px">
