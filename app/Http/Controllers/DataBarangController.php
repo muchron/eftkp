33884f1988bf29572dfa20c31a8a1e81f2a40a6f
msfiqih <msfiqih@gmail.com>
2024-03-19 10:42:21 +0700

    fix pencarian obat

--- app/Http/Controllers/DataBarangController.php
+++ app/Http/Controllers/DataBarangController.php
@@ -11,9 +11,8 @@ class DataBarangController extends Controller
     {
         if ($request) {
             $barang = DataBarang::where('status', 1)
-	            ->where('nama_brng', $request->barang)
 	            ->orWhere('nama_brng', 'like', $request->barang."%")
-	            ->orWhere('letak_barang', 'like', $request->barang . '%')
+	            ->orWhere('letak_barang', 'like', '%'.$request->barang . '%')
                 ->whereHas('jenis', function ($query) {
                     return $query->where('nama', 'not like', '%ALKES%')->where('nama', 'not like', 'logistik');
                 })->orderBy('nama_brng', 'ASC')
