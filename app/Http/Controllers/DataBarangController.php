0c140f7e1f653f025e4fb8948c726cd04d83c85f
msfiqih <msfiqih@gmail.com>
2024-03-19 09:03:56 +0700

    fix search databarang on nama_brng

--- app/Http/Controllers/DataBarangController.php
+++ app/Http/Controllers/DataBarangController.php
@@ -10,7 +10,7 @@ class DataBarangController extends Controller
     function get(Request $request)
     {
         if ($request) {
-            $barang = DataBarang::where('status', 1)->where('nama_brng', 'like', '%' . $request->barang . '%')->orWhere('letak_barang', 'like', '%' . $request->barang . '%')
+            $barang = DataBarang::where('status', 1)->orWhere('nama_brng', 'like', '%' . $request->barang . '%')->orWhere('letak_barang', 'like', '%' . $request->barang . '%')
                 ->whereHas('jenis', function ($query) {
                     return $query->where('nama', 'not like', '%ALKES%')->where('nama', 'not like', 'logistik');
                 })->limit(10)->orderBy('nama_brng', 'ASC')
