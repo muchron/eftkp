3304ad1e279203a40797cee08999a051a7064b93
msfiqih <msfiqih@gmail.com>
2024-10-01 21:36:59 +0700

    fix kunjungan pcare bpjs

--- vendor/aamdsam/bridging-bpjs/src/PCare/Kunjungan.php
+++ vendor/aamdsam/bridging-bpjs/src/PCare/Kunjungan.php
@@ -7,17 +7,17 @@ class Kunjungan extends PcareService
     /**
      * @var string
      */
-    protected $feature = 'kunjungan';
+    protected $feature = 'kunjungan/V1';
 
     public function rujukan($nomorKunjungan)
     {
-        $this->feature .= "/V1/rujukan/{$nomorKunjungan}";
+        $this->feature .= "/rujukan/{$nomorKunjungan}";
         return $this;
     }
 
     public function riwayat($nomorKartu)
     {
-        $this->feature .= "/V1/peserta/{$nomorKartu}";
+        $this->feature .= "/peserta/{$nomorKartu}";
         return $this;
     }
 }
