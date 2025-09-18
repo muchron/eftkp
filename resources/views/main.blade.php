ec96ddd92c4f768a3e2d78b4b9c838809fc8b5fb
msfiqih <msfiqih@gmail.com>
2025-09-18 09:42:16 +0700

    fix url utility

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -276,7 +276,7 @@
     <script src="{{ asset('public/js/select2/select2.min.js') }}"></script>
     <script src="{{ asset('public/js/demo.min.js') }}" defer></script>
     <script src="{{ asset('public/js/tabler.min.js') }}" defer></script>
-    <script src="{{ asset('public/js/utility.js') }}" defer></script>
+    <script src="{{ asset('/public/js/utility.js') }}" defer></script>
     <script src="{{ asset('public/libs/tinymce/tinymce.js') }}" defer></script>
     <script src="{{ asset('public/js/contextMenu/jquery.contextMenu.min.js') }}"></script>
     <script src="{{ asset('public/js/contextMenu/jquery.ui.position.js') }}"></script>
